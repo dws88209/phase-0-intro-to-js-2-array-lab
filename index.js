adcf94592565376fce6e85873268fb3fe8cbecee
Destinee Saunders <dsaunders290@gmail.com>
2022-09-13 15:47:08 -0400

    commit tree af7132cc9fdedea57fad33d805ca2011c9d43dcd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+function destructivelyAppendCat(name){
+    cats.push(name);
+    console.log(cats)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    console.log(cats);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    console.log(cats);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    console.log(cats)
+}
+function appendCat(name){
+    const copyOfCats = [... cats];
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+function prependCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats
+}
+function removeLastCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.pop(name);
+    return copyOfCats
+}
+function removeFirstCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.shift(name);
+    return copyOfCats;
+}
